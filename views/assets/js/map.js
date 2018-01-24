6621e7f24f77fd7c47ee6307371fcc6db6361932
Jessy <jessy@Nina.local>
2018-01-24 01:46:00 +0100

    Ajout d'un niveau de zoom en fonction de la taille de l'écran.

--- views/assets/js/map.js
+++ views/assets/js/map.js
@@ -5,18 +5,26 @@
 
           var latt = Number($('#lat').text());
           var long = Number($('#lng').text());
+          if (screen.width < 1200){
+            var zoom = 8;
+          }
+          else {
+            zoom = 4;
+          }
         }
         else {
           latt = 10;
           long = 10;
+          zoom = 3;
         }
 
-          var latlng = {lat : latt ,lng : long};
 
-          var option = {
-            center : latlng, // récpère les coordonnées gps pour orienter la vue sur le pays consulté.
-            zoom : 3
-          };
+        var latlng = {lat : latt ,lng : long};
+
+        var option = {
+          center : latlng, // récpère les coordonnées gps pour orienter la vue sur le pays consulté.
+          zoom : zoom
+        };
 
       //objet map.
         var map = new google.maps.Map(document.getElementById("map"), option);
